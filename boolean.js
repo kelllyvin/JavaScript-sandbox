f3692de94359bf29bebfb6a32993981bf6e5df1d
kelllyvin <kelvinmarvin666@gmail.com>
2025-03-11 15:47:07 +0100

    updated java

---
+++ boolean.js
@@ -0,0 +1,25 @@
+//BOOOLEANS IS EITHER TRUE OR FALSE
+// COMPARISON OPERATORS < > <= >= == ===(STRICT)
+//LOGICAL OPERATOR AND&& OR|| NOT !
+// FOR AND OPERATORS ALL OPERATORS MUST BE TRUE
+//FOR OR OPERATORS ONLY ONE NEEDS TO TO BE TRUE 
+// FOR NOT OPERATORS IT CAN BE TRUE OF FALSE
+// truth and falsy values
+// string any valid string will be true  can only be false if there its empty
+const myNAme = "jay"
+   console.log(Boolean(myName));
+// numbers any numbers is true either positive or negative only 0 is caled falsy
+ const Value = 0.0;
+ console.log(Boolean(value));
+ 
+
+   
+console.log(5==5.0);
+console.log(5=== );
+
+
+console.log(4.2 >=76 && < 7.8); //f
+console.log(54 === 54.0 ||);
+
+
+
---
+++ null&underfinnd.js
@@ -0,0 +1,6 @@
+// NULL and UNDERFIND is also falsy value 
+let user = null; // is a falsy value
+// a  b c d 
+
+//underfind 
+let myNAme; // falsy value 
